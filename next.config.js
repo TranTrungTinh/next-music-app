55dd9166929dfc7254e4a4d9df6978bcb5e6ecb8
TinhTran <trantrungtinh4954@gmail.com>
2023-06-13 21:37:49 +0700

    feat: :lipstick: Add UI sidebar, UI home page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'misc.scdn.co', 
+      'i.scdn.co', 
+      'media.pitchfork.com', 
+      'i1.sndcdn.com',
+      'geo-media.beatsource.com'
+    ],
+  }
+}
 
 module.exports = nextConfig
